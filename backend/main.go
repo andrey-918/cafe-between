944529811851ada34c7c8bbfe4920b7f367287dd
andreykarganov <karganov.an@yandex.ru>
2025-11-17 15:03:34 +0300

    bug fix

--- backend/main.go
+++ backend/main.go
@@ -24,7 +24,7 @@ func main() {
 	// CORS middleware
 	r.Use(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Access-Control-Allow-Origin", "https://localhost")
+			w.Header().Set("Access-Control-Allow-Origin", "https://5184011c4f6d.vps.myjino.ru")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Authorization")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
